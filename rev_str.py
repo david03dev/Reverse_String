1abedce54311767886770a7b758f3ed6c4c62e84
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2024-07-30 22:27:20 +0530

    Create rev_str.py

---
+++ rev_str.py
@@ -0,0 +1,8 @@
+# Read the input string
+input_string = input().strip()
+
+# Reverse the string using slicing
+reversed_string = input_string[::-1]
+
+# Print the reversed string
+print(reversed_string)
